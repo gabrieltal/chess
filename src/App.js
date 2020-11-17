1bc9395711cde4fcb84b0a8764706899b703526e
Gabriel <gabrielvt6@gmail.com>
2020-11-17 12:04:16 -0800

    Minor spacing update, updated classes to be more utility based and cleaned up stylesheet

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Game from './components/game';
 function App() {
   return (
     <div className="app">
-      <header className="d-flex justify-content-between">
+      <header className="d-flex justify-content-between bg-black">
         <nav>
-          <a href="https://github.com/gabrieltal/chess">Github</a>
+          <a href="https://github.com/gabrieltal/chess" target="_blank" rel="nofollow">Github</a>
         </nav>
         <h1 class="m-0">Chess</h1>
-        <p className="font-md">A <a className="white" href="https://github.com/in/gvtalavera">Gabriel Talavera</a> production.</p>
+        <p className="font-md">A <a className="white" href="https://github.com/in/gvtalavera" target="_blank" rel="nofollow">Gabriel Talavera</a> production.</p>
       </header>
-      <main className="d-flex">
+      <main className="d-flex flex-column align-items-center vw-100">
         <Game />
       </main>
     </div>
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -10,101 +10,91 @@ export default class Board extends React.Component {
     return (
       <div className="board">
         <div className="row">
-        {this.renderSquare(0, 7)}
-        {this.renderSquare(1, 7)}
-        {this.renderSquare(2, 7)}
-        {this.renderSquare(3, 7)}
-        {this.renderSquare(4, 7)}
-        {this.renderSquare(5, 7)}
-        {this.renderSquare(6, 7)}
-        {this.renderSquare(7, 7)}
+          {this.renderSquare(0, 7)}
+          {this.renderSquare(1, 7)}
+          {this.renderSquare(2, 7)}
+          {this.renderSquare(3, 7)}
+          {this.renderSquare(4, 7)}
+          {this.renderSquare(5, 7)}
+          {this.renderSquare(6, 7)}
+          {this.renderSquare(7, 7)}
         </div>
 
         <div className="row">
-        {this.renderSquare(0, 6)}
-        {this.renderSquare(1, 6)}
-        {this.renderSquare(2, 6)}
-        {this.renderSquare(3, 6)}
-        {this.renderSquare(4, 6)}
-        {this.renderSquare(5, 6)}
-        {this.renderSquare(6, 6)}
-        {this.renderSquare(7, 6)}
+          {this.renderSquare(0, 6)}
+          {this.renderSquare(1, 6)}
+          {this.renderSquare(2, 6)}
+          {this.renderSquare(3, 6)}
+          {this.renderSquare(4, 6)}
+          {this.renderSquare(5, 6)}
+          {this.renderSquare(6, 6)}
+          {this.renderSquare(7, 6)}
         </div>
 
         <div className="row">
-
-        {this.renderSquare(0, 5)}
-        {this.renderSquare(1, 5)}
-        {this.renderSquare(2, 5)}
-        {this.renderSquare(3, 5)}
-        {this.renderSquare(4, 5)}
-        {this.renderSquare(5, 5)}
-        {this.renderSquare(6, 5)}
-        {this.renderSquare(7, 5)}
-
-
-
+          {this.renderSquare(0, 5)}
+          {this.renderSquare(1, 5)}
+          {this.renderSquare(2, 5)}
+          {this.renderSquare(3, 5)}
+          {this.renderSquare(4, 5)}
+          {this.renderSquare(5, 5)}
+          {this.renderSquare(6, 5)}
+          {this.renderSquare(7, 5)}
         </div>
 
         <div className="row">
-        {this.renderSquare(0, 4)}
-        {this.renderSquare(1, 4)}
-        {this.renderSquare(2, 4)}
-        {this.renderSquare(3, 4)}
-        {this.renderSquare(4, 4)}
-        {this.renderSquare(5, 4)}
-        {this.renderSquare(6, 4)}
-        {this.renderSquare(7, 4)}
-
+          {this.renderSquare(0, 4)}
+          {this.renderSquare(1, 4)}
+          {this.renderSquare(2, 4)}
+          {this.renderSquare(3, 4)}
+          {this.renderSquare(4, 4)}
+          {this.renderSquare(5, 4)}
+          {this.renderSquare(6, 4)}
+          {this.renderSquare(7, 4)}
         </div>
 
         <div className="row">
-
-        {this.renderSquare(0, 3)}
-        {this.renderSquare(1, 3)}
-        {this.renderSquare(2, 3)}
-        {this.renderSquare(3, 3)}
-        {this.renderSquare(4, 3)}
-        {this.renderSquare(5, 3)}
-        {this.renderSquare(6, 3)}
-        {this.renderSquare(7, 3)}
+          {this.renderSquare(0, 3)}
+          {this.renderSquare(1, 3)}
+          {this.renderSquare(2, 3)}
+          {this.renderSquare(3, 3)}
+          {this.renderSquare(4, 3)}
+          {this.renderSquare(5, 3)}
+          {this.renderSquare(6, 3)}
+          {this.renderSquare(7, 3)}
         </div>
 
         <div className="row">
-        {this.renderSquare(0, 2)}
-        {this.renderSquare(1, 2)}
-        {this.renderSquare(2, 2)}
-        {this.renderSquare(3, 2)}
-        {this.renderSquare(4, 2)}
-        {this.renderSquare(5, 2)}
-        {this.renderSquare(6, 2)}
-        {this.renderSquare(7, 2)}
+          {this.renderSquare(0, 2)}
+          {this.renderSquare(1, 2)}
+          {this.renderSquare(2, 2)}
+          {this.renderSquare(3, 2)}
+          {this.renderSquare(4, 2)}
+          {this.renderSquare(5, 2)}
+          {this.renderSquare(6, 2)}
+          {this.renderSquare(7, 2)}
         </div>
 
         <div className="row">
-
-
-        {this.renderSquare(0, 1)}
-        {this.renderSquare(1, 1)}
-        {this.renderSquare(2, 1)}
-        {this.renderSquare(3, 1)}
-        {this.renderSquare(4, 1)}
-        {this.renderSquare(5, 1)}
-        {this.renderSquare(6, 1)}
-        {this.renderSquare(7, 1)}
+          {this.renderSquare(0, 1)}
+          {this.renderSquare(1, 1)}
+          {this.renderSquare(2, 1)}
+          {this.renderSquare(3, 1)}
+          {this.renderSquare(4, 1)}
+          {this.renderSquare(5, 1)}
+          {this.renderSquare(6, 1)}
+          {this.renderSquare(7, 1)}
         </div>
 
         <div className="row">
-
-
-        {this.renderSquare(0, 0)}
-        {this.renderSquare(1, 0)}
-        {this.renderSquare(2, 0)}
-        {this.renderSquare(3, 0)}
-        {this.renderSquare(4, 0)}
-        {this.renderSquare(5, 0)}
-        {this.renderSquare(6, 0)}
-        {this.renderSquare(7, 0)}
+          {this.renderSquare(0, 0)}
+          {this.renderSquare(1, 0)}
+          {this.renderSquare(2, 0)}
+          {this.renderSquare(3, 0)}
+          {this.renderSquare(4, 0)}
+          {this.renderSquare(5, 0)}
+          {this.renderSquare(6, 0)}
+          {this.renderSquare(7, 0)}
         </div>
       </div>
     );
