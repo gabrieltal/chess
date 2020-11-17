a66573651ee2149581d6573bd7efdccf98348637
Gabriel <gabrielvt6@gmail.com>
2020-11-17 12:14:41 -0800

    Used noreferrer when using target blank as well as noopener

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
     <div className="app">
       <header className="d-flex justify-content-between bg-black p-2 align-items-center white">
         <nav>
-          <a href="https://github.com/gabrieltal/chess" target="_blank" rel="nofollow">Github</a>
+          <a href="https://github.com/gabrieltal/chess" target="_blank" rel="noopener noreferrer">Github</a>
         </nav>
         <h1 class="m-0">Chess</h1>
-        <p className="font-md">A <a className="white" href="https://github.com/in/gvtalavera" target="_blank" rel="nofollow">Gabriel Talavera</a> production.</p>
+        <p className="font-md">A <a className="white" href="https://github.com/in/gvtalavera" target="_blank" rel="noopener noreferrer">Gabriel Talavera</a> production.</p>
       </header>
       <main className="d-flex flex-column align-items-center vw-100">
         <Game />
