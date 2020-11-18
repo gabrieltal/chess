d61feee2363a48db8b581cbb8a13c1704e9375ad
Gabriel <gabrielvt6@gmail.com>
2020-11-18 11:23:46 -0800

    Updated board to render the pieces initial positions

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,34 +1,40 @@
 import Square from './square';
 import React from 'react';
+import Knight from './pieces/knight';
+import Bishop from './pieces/bishop';
+import King from './pieces/king';
+import Queen from './pieces/queen';
+import Rook from './pieces/rook';
+import Pawn from './pieces/pawn';
 
 export default class Board extends React.Component {
-  renderSquare(x, y) {
-    return <Square x={x} y={y} />;
+  renderSquare(x, y, piece = null) {
+    return <Square x={x} y={y} piece={piece} />;
   }
 
   render() {
     return (
       <div className="board m-2">
         <div className="row">
-          {this.renderSquare(0, 7)}
-          {this.renderSquare(1, 7)}
-          {this.renderSquare(2, 7)}
-          {this.renderSquare(3, 7)}
-          {this.renderSquare(4, 7)}
-          {this.renderSquare(5, 7)}
-          {this.renderSquare(6, 7)}
-          {this.renderSquare(7, 7)}
+          {this.renderSquare(0, 7, new Rook('black'))}
+          {this.renderSquare(1, 7, new Knight('black'))}
+          {this.renderSquare(2, 7, new Bishop('black'))}
+          {this.renderSquare(3, 7, new Queen('black'))}
+          {this.renderSquare(4, 7, new King('black'))}
+          {this.renderSquare(5, 7, new Bishop('black'))}
+          {this.renderSquare(6, 7, new Knight('black'))}
+          {this.renderSquare(7, 7, new Rook('black'))}
         </div>
 
         <div className="row">
-          {this.renderSquare(0, 6)}
-          {this.renderSquare(1, 6)}
-          {this.renderSquare(2, 6)}
-          {this.renderSquare(3, 6)}
-          {this.renderSquare(4, 6)}
-          {this.renderSquare(5, 6)}
-          {this.renderSquare(6, 6)}
-          {this.renderSquare(7, 6)}
+          {this.renderSquare(0, 6, new Pawn('black'))}
+          {this.renderSquare(1, 6, new Pawn('black'))}
+          {this.renderSquare(2, 6, new Pawn('black'))}
+          {this.renderSquare(3, 6, new Pawn('black'))}
+          {this.renderSquare(4, 6, new Pawn('black'))}
+          {this.renderSquare(5, 6, new Pawn('black'))}
+          {this.renderSquare(6, 6, new Pawn('black'))}
+          {this.renderSquare(7, 6, new Pawn('black'))}
         </div>
 
         <div className="row">
@@ -76,25 +82,25 @@ export default class Board extends React.Component {
         </div>
 
         <div className="row">
-          {this.renderSquare(0, 1)}
-          {this.renderSquare(1, 1)}
-          {this.renderSquare(2, 1)}
-          {this.renderSquare(3, 1)}
-          {this.renderSquare(4, 1)}
-          {this.renderSquare(5, 1)}
-          {this.renderSquare(6, 1)}
-          {this.renderSquare(7, 1)}
+          {this.renderSquare(0, 1, new Pawn('white'))}
+          {this.renderSquare(1, 1, new Pawn('white'))}
+          {this.renderSquare(2, 1, new Pawn('white'))}
+          {this.renderSquare(3, 1, new Pawn('white'))}
+          {this.renderSquare(4, 1, new Pawn('white'))}
+          {this.renderSquare(5, 1, new Pawn('white'))}
+          {this.renderSquare(6, 1, new Pawn('white'))}
+          {this.renderSquare(7, 1, new Pawn('white'))}
         </div>
 
         <div className="row">
-          {this.renderSquare(0, 0)}
-          {this.renderSquare(1, 0)}
-          {this.renderSquare(2, 0)}
-          {this.renderSquare(3, 0)}
-          {this.renderSquare(4, 0)}
-          {this.renderSquare(5, 0)}
-          {this.renderSquare(6, 0)}
-          {this.renderSquare(7, 0)}
+          {this.renderSquare(0, 0, new Rook('white'))}
+          {this.renderSquare(1, 0, new Knight('white'))}
+          {this.renderSquare(2, 0, new Bishop('white'))}
+          {this.renderSquare(3, 0, new Queen('white'))}
+          {this.renderSquare(4, 0, new King('white'))}
+          {this.renderSquare(5, 0, new Bishop('white'))}
+          {this.renderSquare(6, 0, new Knight('white'))}
+          {this.renderSquare(7, 0, new Rook('white'))}
         </div>
       </div>
     );
