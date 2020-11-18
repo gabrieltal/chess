158deda81c525f2fb22eabfb040edf05d07e6aee
Gabriel <gabrielvt6@gmail.com>
2020-11-18 11:23:13 -0800

    Added a constructor to square component and added logic to display a piece if piece is present

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 export default class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      piece: props.piece
+    }
+  }
+
   chooseColor(x, y) {
     if (x % 2 === 0) {
       return y % 2 === 0 ? 'bg-dark' : 'bg-light';
@@ -11,7 +18,8 @@ export default class Square extends React.Component {
 
   render() {
     return (
-      <div className={`square ${this.chooseColor(this.props.x, this.props.y)}` } onClick={ () => alert('click') }>
+      <div className={`square ${this.chooseColor(this.props.x, this.props.y)}` }>
+        <img src={this.state.piece ? this.state.piece.image : null} />
       </div>
     );
   }
