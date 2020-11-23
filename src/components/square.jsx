024c8313376c67806ef77b975769845d11a16cc6
Gabriel <gabrielvt6@gmail.com>
2020-11-23 11:13:24 -0800

    Updated square component to determine if the piece was selected to add a yellow border

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 
 export default class Square extends React.Component {
-  squareColor(x, y) {
+  squareColor(x, y, piece) {
+    let colors;
+
     if (x % 2 === 0) {
-      return y % 2 === 0 ? 'bg-dark' : 'bg-light';
+      colors = y % 2 === 0 ? 'bg-dark' : 'bg-light';
     } else {
-      return y % 2 === 0 ? 'bg-light' : 'bg-dark';
+      colors = y % 2 === 0 ? 'bg-light' : 'bg-dark';
     }
+
+    if (piece && piece.selected) {
+      colors += ' border-yellow';
+    }
+
+    return colors;
   }
 
   render() {
     if (this.props.piece) {
       return (
-        <div className={`square ${this.squareColor(this.props.x, this.props.y)}`} onClick={this.props.onClick}>
+        <div className={`square ${this.squareColor(this.props.x, this.props.y, this.props.piece)}`} onClick={this.props.onClick}>
           <img src={this.props.piece.image} alt={this.props.piece.name}/>
         </div>
       );
