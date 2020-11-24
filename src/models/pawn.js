4a3176b72de2507e8720cf6c3ff58de0da44a4bc
Gabriel <gabrielvt6@gmail.com>
2020-11-23 16:25:05 -0800

    Added Pawn movement methods

--- src/models/pawn.js
+++ src/models/pawn.js
@@ -15,6 +15,36 @@ export default class Pawn {
   possibleMoves() {
     var posibilities = [];
 
+    posibilities.push(this.currentPosition + this.movementByColor());
+
+    if (!this.hasMoved) {
+      posibilities.push(this.currentPosition + this.movementByColor() + this.movementByColor());
+    }
+
     return posibilities;
   }
+
+  movementByColor() {
+    // White pieces move up the board
+    if (this.color === 'white') {
+      return -8;
+    // Black pieces move down the board
+    } else {
+      return 8;
+    }
+  }
+
+  makeMove(index) {
+    this.currentPosition = index;
+    this.hasMoved = true;
+  }
+
+  // inRangeToStrike(board) {
+  //   let squaresInRangeOfStrike = [];
+  //
+  //   squaresInRangeOfStrike.push(this.currentPosition + this.movementByColor() + 1);
+  //   squaresInRangeOfStrike.push(this.currentPosition + this.movementByColor() - 1);
+  //
+  //   board.checkSquareForEnemies(this.color, squaresInRangeOfStrike);
+  // }
 }
