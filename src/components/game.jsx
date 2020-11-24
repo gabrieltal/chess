2d663ef185f590d4fd393897c415eb35b8e79c2a
Gabriel <gabrielvt6@gmail.com>
2020-11-23 16:25:29 -0800

    Updated piece initialization to set currentPosition

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -29,44 +29,44 @@ export default class Game extends React.Component {
     let squares = Array(64).fill(null);
 
     // Black pieces
-    squares[0] = new Rook('black');
-    squares[1] = new Knight('black');
-    squares[2] = new Bishop('black');
-    squares[3] = new Queen('black');
-    squares[4] = new King('black');
-    squares[5] = new Bishop('black');
-    squares[6] = new Knight('black');
-    squares[7] = new Rook('black');
+    squares[0] = new Rook('black', 0);
+    squares[1] = new Knight('black', 1);
+    squares[2] = new Bishop('black', 2);
+    squares[3] = new Queen('black', 3);
+    squares[4] = new King('black', 4);
+    squares[5] = new Bishop('black', 5);
+    squares[6] = new Knight('black', 6);
+    squares[7] = new Rook('black', 7);
 
     // Black pawns
-    squares[8]= new Pawn('black');
-    squares[9]= new Pawn('black');
-    squares[10]= new Pawn('black');
-    squares[11]= new Pawn('black');
-    squares[12]= new Pawn('black');
-    squares[13]= new Pawn('black');
-    squares[14]= new Pawn('black');
-    squares[15]= new Pawn('black');
+    squares[8]= new Pawn('black', 8);
+    squares[9]= new Pawn('black', 9);
+    squares[10]= new Pawn('black', 10);
+    squares[11]= new Pawn('black', 11);
+    squares[12]= new Pawn('black', 12);
+    squares[13]= new Pawn('black', 13);
+    squares[14]= new Pawn('black', 14);
+    squares[15]= new Pawn('black', 15);
 
     // White pieces
-    squares[56] = new Rook('white');
-    squares[57] = new Knight('white');
-    squares[58] = new Bishop('white');
-    squares[59] = new Queen('white');
-    squares[60] = new King('white');
-    squares[61] = new Bishop('white');
-    squares[62] = new Knight('white');
-    squares[63] = new Rook('white')
+    squares[56] = new Rook('white', 56);
+    squares[57] = new Knight('white', 57);
+    squares[58] = new Bishop('white', 58);
+    squares[59] = new Queen('white', 59);
+    squares[60] = new King('white', 60);
+    squares[61] = new Bishop('white', 61);
+    squares[62] = new Knight('white', 62);
+    squares[63] = new Rook('white', 63)
 
     // White pawns
-    squares[48] = new Pawn('white');
-    squares[49] = new Pawn('white');
-    squares[50] = new Pawn('white');
-    squares[51] = new Pawn('white');
-    squares[52] = new Pawn('white');
-    squares[53] = new Pawn('white');
-    squares[54] = new Pawn('white');
-    squares[55] = new Pawn('white');
+    squares[48] = new Pawn('white', 48);
+    squares[49] = new Pawn('white', 49);
+    squares[50] = new Pawn('white', 50);
+    squares[51] = new Pawn('white', 51);
+    squares[52] = new Pawn('white', 52);
+    squares[53] = new Pawn('white', 53);
+    squares[54] = new Pawn('white', 54);
+    squares[55] = new Pawn('white', 55);
 
     return squares;
   }
