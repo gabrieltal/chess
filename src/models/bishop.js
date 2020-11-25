86b35fedd327baf6e64a4a83f8222d471ee5cf74
Gabriel <gabrielvt6@gmail.com>
2020-11-25 12:38:56 -0800

    Added movement for bishops

--- src/models/bishop.js
+++ src/models/bishop.js
@@ -8,13 +8,100 @@ export default class Bishop {
     this.currentPosition = position;
   }
 
-  isValidMove(move) {
-    this.possibleMoves().includes(move);
+  possibleMoves(squares) {
+    const possibilities = [];
+    let possibleMove;
+    let pieceAtSquare;
+
+    // Add moves from the top left diagonal
+    possibleMove = this.currentPosition;
+    while(!this.atLeftBorder(possibleMove) && !this.atTopBorder(possibleMove)) {
+      possibleMove -= 9;
+
+      pieceAtSquare = squares[possibleMove];
+      if (pieceAtSquare && pieceAtSquare.color !== this.color) {
+        possibilities.push(possibleMove);
+        break;
+      } else if (pieceAtSquare) {
+        break;
+      } else {
+        possibilities.push(possibleMove);
+      }
+    }
+
+    // Add moves from the top right diagonal
+    possibleMove = this.currentPosition;
+    while(!this.atRightBorder(possibleMove) && !this.atTopBorder(possibleMove)) {
+      possibleMove -= 7;
+
+      pieceAtSquare = squares[possibleMove];
+      if (pieceAtSquare && pieceAtSquare.color !== this.color) {
+        possibilities.push(possibleMove);
+        break;
+      } else if (pieceAtSquare) {
+        break;
+      } else {
+        possibilities.push(possibleMove);
+      }
+    }
+
+    // Add moves from the bottom left diagonal
+    possibleMove = this.currentPosition;
+    while(!this.atLeftBorder(possibleMove) && !this.atBottomBorder(possibleMove)) {
+      possibleMove += 7;
+
+      pieceAtSquare = squares[possibleMove];
+      if (pieceAtSquare && pieceAtSquare.color !== this.color) {
+        possibilities.push(possibleMove);
+        break;
+      } else if (pieceAtSquare) {
+        break;
+      } else {
+        possibilities.push(possibleMove);
+      }
+    }
+
+    // Add moves from the bottom right diagonal
+    possibleMove = this.currentPosition;
+    while(!this.atRightBorder(possibleMove) && !this.atBottomBorder(possibleMove)) {
+      possibleMove += 9;
+
+      pieceAtSquare = squares[possibleMove];
+      if (pieceAtSquare && pieceAtSquare.color !== this.color) {
+        possibilities.push(possibleMove);
+        break;
+      } else if (pieceAtSquare) {
+        break;
+      } else {
+        possibilities.push(possibleMove);
+      }
+    }
+
+    return possibilities;
   }
 
-  possibleMoves() {
-    var posibilities = [];
+  atLeftBorder(position) {
+    return position % 8 === 0;
+  }
 
-    return posibilities;
+  atRightBorder(position) {
+    return (position + 1) % 8 === 0;
+  }
+
+  atTopBorder(position) {
+    return position < 0;
+  }
+
+  atBottomBorder(position) {
+    return position > 63;
+  }
+
+  teamPieceAtSquare(squares, position) {
+    return squares[position] && squares[position].color === this.color;
+  }
+
+  makeMove(index) {
+    this.currentPosition = index;
+    this.hasMoved = true;
   }
 }
