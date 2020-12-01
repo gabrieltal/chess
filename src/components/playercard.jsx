90281c865763fcc6a0323c31742084732f6ce4e1
Gabriel <gabrielvt6@gmail.com>
2020-12-01 14:39:51 -0800

    Minor update to use bg-grey for users if it isnt their turn

--- src/components/playercard.jsx
+++ src/components/playercard.jsx
@@ -6,7 +6,7 @@ export default class PlayerCard extends React.Component {
       <div className="d-block p-2">
         <div className="player-card bg-white d-flex align-items-start justify-content-between p-1">
           <h2 className="m-0 capitalize">{this.props.player.color}</h2>
-          <span className={`bg-grey p-1 white badge ${this.props.current === this.props.player ? 'active' : 'd-none' }`}>Your turn</span>
+          <span className={`bg-grey p-1 white badge ${this.props.current === this.props.player ? 'bg-blue' : 'bg-grey' }`}>Your turn</span>
         </div>
         <div className="graveyard">
         </div>
