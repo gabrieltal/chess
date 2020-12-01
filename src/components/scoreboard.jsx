42d0441aad6e378d008676b5b672bf4eaf5c300f
Gabriel <gabrielvt6@gmail.com>
2020-12-01 13:28:54 -0800

    Moved chess notation logic to the history model, cleaned up logic to display moves

--- src/components/scoreboard.jsx
+++ src/components/scoreboard.jsx
@@ -3,84 +3,27 @@ import React from 'react';
 import PlayerCard from './playercard';
 
 export default class Scoreboard extends React.Component {
-  chessNotation(piece, finish) {
-    let x, y;
-    const PIECE_DICTIONARY = {
-      Pawn: '',
-      King: 'K',
-      Queen: 'Q',
-      Bishop: 'B',
-      Knight: 'N',
-      Rook: 'R'
-    };
-
-    if (finish % 8 === 0) {
-      x = 'a';
-    } else if ((finish - 1) % 8 === 0) {
-      x = 'b';
-    } else if ((finish - 2) % 8 === 0) {
-      x = 'c';
-    } else if ((finish - 3) % 8 === 0) {
-      x = 'd';
-    } else if ((finish - 4) % 8 === 0) {
-      x = 'e';
-    } else if ((finish - 5) % 8 === 0) {
-      x = 'f';
-    } else if ((finish - 6) % 8 === 0) {
-      x = 'g';
-    } else {
-      x = 'h';
-    }
-
-    if (finish < 8) {
-      y = '8';
-    } else if (finish < 16) {
-      y = '7';
-    } else if (finish < 24) {
-      y = '6';
-    } else if (finish < 32) {
-      y = '5';
-    } else if (finish < 40) {
-      y = '4';
-    } else if (finish < 48) {
-      y = '3';
-    } else if (finish < 56) {
-      y = '2';
-    } else {
-      y = '1';
-    }
-
-    return <span>{PIECE_DICTIONARY[piece.constructor.name]}{x}{y}</span>;
-  }
-
   displayMoveList(moves) {
     const moveList = [];
     let whiteMove, blackMove;
 
     for (let i = 0; i < moves.length; i++) {
       whiteMove = moves[i];
+      blackMove = null;
 
-      if (i === (moves.length - 1)) {
-        moveList.push(
-          <li key={i}>
-            <div className="move-description">
-              {this.chessNotation(whiteMove.piece, whiteMove.move_to)}
-            </div>
-          </li>
-        );
-      } else {
+      if (i !== (moves.length - 1)) {
         i += 1;
         blackMove = moves[i];
-
-        moveList.push(
-          <li key={i}>
-            <div className="move-description">
-              {this.chessNotation(whiteMove.piece, whiteMove.move_to)}
-              {this.chessNotation(blackMove.piece, blackMove.move_to)}
-            </div>
-          </li>
-        );
       }
+
+      moveList.push(
+        <li key={i}>
+          <div className="move-description">
+            <span>{whiteMove.notation}</span>
+            <span>{blackMove?.notation}</span>
+          </div>
+        </li>
+      );
     }
 
     return moveList;
--- src/models/history.js
+++ src/models/history.js
@@ -4,11 +4,65 @@ export default class History {
   }
 
   logMove(description) {
+    description['notation'] = this.translateToChessNotation(
+      description['piece'],
+      description['move_to']
+    );
     this.moves.push(description);
     return description;
   }
 
   lastMove() {
     return this.moves[this.moves.length - 1];
+  }
+
+  translateToChessNotation(piece, movedTo) {
+    let x, y;
+    const PIECE_DICTIONARY = {
+      Pawn: '',
+      King: 'K',
+      Queen: 'Q',
+      Bishop: 'B',
+      Knight: 'N',
+      Rook: 'R'
+    };
+
+    if (movedTo % 8 === 0) {
+      x = 'a';
+    } else if ((movedTo - 1) % 8 === 0) {
+      x = 'b';
+    } else if ((movedTo - 2) % 8 === 0) {
+      x = 'c';
+    } else if ((movedTo - 3) % 8 === 0) {
+      x = 'd';
+    } else if ((movedTo - 4) % 8 === 0) {
+      x = 'e';
+    } else if ((movedTo - 5) % 8 === 0) {
+      x = 'f';
+    } else if ((movedTo - 6) % 8 === 0) {
+      x = 'g';
+    } else {
+      x = 'h';
+    }
+
+    if (movedTo < 8) {
+      y = '8';
+    } else if (movedTo < 16) {
+      y = '7';
+    } else if (movedTo < 24) {
+      y = '6';
+    } else if (movedTo < 32) {
+      y = '5';
+    } else if (movedTo < 40) {
+      y = '4';
+    } else if (movedTo < 48) {
+      y = '3';
+    } else if (movedTo < 56) {
+      y = '2';
+    } else {
+      y = '1';
+    }
+
+    return `${PIECE_DICTIONARY[piece.constructor.name]}${x}${y}`;
   }
 }
