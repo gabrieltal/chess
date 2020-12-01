d0160485dcde81e7b6e7d17c4385381080e3333e
Gabriel <gabrielvt6@gmail.com>
2020-12-01 15:41:41 -0800

    Minor clean up to the playercard flex flow

--- src/components/playercard.jsx
+++ src/components/playercard.jsx
@@ -3,10 +3,14 @@ import React from 'react';
 export default class PlayerCard extends React.Component {
   render() {
     return (
-        <div className="player-card bg-white d-flex align-items-start justify-content-between p-1 m-2">
-          <h2 className="m-0 capitalize">{this.props.player.color}</h2>
-          <span className={`bg-grey p-1 white badge ${this.props.current === this.props.player ? 'bg-blue' : 'bg-grey' }`}>Your turn</span>
+        <div className="player-card bg-white d-flex flex-column p-1 m-2">
+          <div className="d-flex justify-content-between">
+            <h2 className="m-0 capitalize">{this.props.player.color}</h2>
+            <span className={`bg-grey p-1 white badge ${this.props.current === this.props.player ? 'bg-blue' : 'bg-grey' }`}>Your turn</span>
+          </div>
+
           <div className="graveyard">
+            {this.props.graveyard}
           </div>
         </div>
     );
