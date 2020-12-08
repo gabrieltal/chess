f8fd4e047297f0402532d6970a627fdb87e765b1
Gabriel <gabrielvt6@gmail.com>
2020-12-07 17:49:12 -0800

    Updated failing test so it passes

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './app';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Chess');
   expect(linkElement).toBeInTheDocument();
 });
