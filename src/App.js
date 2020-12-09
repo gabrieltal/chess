d0d90d7421053b65a6b5e15033cc65442b753712
Gabriel <gabrielvt6@gmail.com>
2020-12-08 16:30:51 -0800

    Minor clean up to verbiage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <nav className="d-grid nav-grid align-items-center">
           <a className="hide-on-sm font-md" href="https://github.com/gabrieltal/chess" target="_blank" rel="noopener noreferrer">Github</a>
           <div className="d-flex justify-content-center"><h1 className="m-0">Chess</h1></div>
-          <div className="d-flex justify-content-end hide-on-sm"><p className="font-md">A <a className="white" href="https://linkedin.com/in/gvtalavera" target="_blank" rel="noopener noreferrer">Gabriel Talavera</a> production.</p></div>
+          <a className="justify-self-end hide-on-sm font-md" href="https://linkedin.com/in/gvtalavera" target="_blank" rel="noopener noreferrer">Linkedin</a>
         </nav>
       </header>
       <Game />
